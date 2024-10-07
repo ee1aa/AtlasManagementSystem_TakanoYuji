e3a60414750eb8c2e184acdabb9f25bd7f100aa7
kotatumikan <1471033@gmail.com>
2024-10-07 22:39:09 +0900

    初期データ準備中

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,6 +12,16 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-
+        DB::table('users')->insert([
+            ['over_name' => 'Atlas',
+            'under_name' => '一郎',
+            'over_name_kana' => 'アトラス',
+            'under_name_kana' => 'イチロウ',
+            'mail_address' => 'atlas1@gmail.com',
+            'sex' => '男',
+            'birth_day' => '1986-08-01',
+            'role' => '',
+            'password' => 'password1',],
+        ]);
     }
 }
